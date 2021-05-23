70e2b5b816c3b24465b219a52488a962eb90a6b8
sudharajendran0 <sudharajendran0108@gmail.com>
2021-05-23 16:42:20 +0530

    build

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { ResizedEvent } from 'angular-resize-event';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
